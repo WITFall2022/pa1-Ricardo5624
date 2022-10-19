ebea3337f81bcf039579eae224851733f2c4bd06
333815 <333815@664-161464-lab.schools.mybps.org>
2022-10-19 11:33:39 -0400

    Professor, I added my code from last week.

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -1,10 +1,26 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA1a {
 
 	// TODO: document this function
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub
+		
+	Scanner input = new Scanner(System.in);
+	
+	int a, b, c;	
+	double average;
+	
+	System.out.printf("Enter three intergers; ");
+	a = input.nextInt();
+	b = input.nextInt();
+	c = input.nextInt();
+	average = (a + b + c) / 3.0;
+	System.out.printf("The average is %f%n", average);
+
 		// TODO: write your code here
 	}
 
